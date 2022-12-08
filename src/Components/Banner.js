0778f35dac2abd28e43b1bd964cc898e7a8d4d02
narendra <you@example.com>
2022-12-08 14:53:00 +0530

    update-slide

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -18,9 +18,9 @@ const Banner = () => {
           <div class="carousel-item">
             <img src="./images/3.png" class="d-block w-100" data-bs-interval="4000" alt="..." />
           </div>
-          {/* <div class="carousel-item">
-            <img src="./images/Seedling_4.png"  data-bs-interval="4000" class="d-block w-100" alt="..." />
-          </div> */}
+          <div class="carousel-item">
+            <img src="./images/slide4.png"  data-bs-interval="4000" class="d-block w-100" alt="..."/>
+          </div>
         </div>
         <button
           class="carousel-control-prev"
